6ecb93ec4177481fed326e196129cfdd034e6549
SANTIAGO ALONSO <SANTIAGO.RALONSO@correo.ucu.edu.uy>
2021-10-19 00:26:17 -0300

    añadido modificador  de nombre para varios pipes que guardan progreso

--- src/Library/Filters/FIlterShowProgress.cs
+++ src/Library/Filters/FIlterShowProgress.cs
@@ -2,20 +2,20 @@ namespace CompAndDel
 {
     public class SaveProgress : IFilter
     {
-        private static int numProgreso 
+        private static int numProgreso = 0;
+        private static int NumProgreso 
         {
             get
             {
                 return numProgreso+=1;
             }
-            set{}
         }
         //la responsabilidad de guardar una imagen en disco ya la tiene provider, por lo que creo una clase que implemente
         //ifilter para poder pasar este proceso como parametro "filtro"
         public IPicture Filter(IPicture image)
         {
             PictureProvider tempProv = new PictureProvider();
-            tempProv.SavePicture(image, @"PipeIntermedio{SaveProgress.numProgreso}.jpg");
+            tempProv.SavePicture(image, @"PipeIntermedio" + (SaveProgress.NumProgreso) + ".jpg");
 
             return image;
         }
